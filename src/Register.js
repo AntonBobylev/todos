13b4b1541a2925443f431073f0a0cad8b3587a9e
AntonBobylev <ya-anton3632@yandex.ru>
2024-04-08 00:04:24 +0300

    revert last commit

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,6 @@
 import {Component} from 'react';
 import {Navigate} from 'react-router-dom';
-import {register} from './api';
+import {register} from './auth';
 
 export default class Register extends Component
 {
