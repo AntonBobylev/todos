d81cf9d1a2b846d1afec35d1b963755adae09194
AntonBobylev <ya-anton3632@yandex.ru>
2024-04-09 00:03:19 +0300

    index.html styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bulma/css/bulma.css';
-import App from './App';
-import './backend/firebase';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import App from './App';
+
+import './backend/firebase';
+import 'bulma/css/bulma.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
